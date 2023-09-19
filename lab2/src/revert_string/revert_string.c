42904cbb60f0e67db44b4c1dd4f4e86dc5c27f72
Vuzhos <31344427+Vuzhos@users.noreply.github.com>
2023-09-19 11:20:01 +0000

    выполнено задание 2 лабы

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,21 @@
 
 void RevertString(char *str)
 {
-	// your code here
-}
+	int length = 0;
+	while (*(str + length) != '\0')
+	{
+		length++;
+	}
 
+	char* reverted = (char*) malloc(length * sizeof(char));
+
+	int i = 0;
+	for (char* p = str + length - 1; p >= str; p--, i++)
+	{
+		*(reverted + i) = *p;
+	}
+	for (int i = 0; i < length; i++)
+	{
+		str[i] = reverted[i];
+	}
+}
