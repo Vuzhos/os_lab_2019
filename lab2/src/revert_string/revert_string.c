a30147e1bffa2ef4d423120ad1fbfdcb4269a82a
DmitryKhvoshchevskiy <144114741+DmitryKhvoshchevskiy@users.noreply.github.com>
2023-10-07 09:58:15 +0000

    изменены swap.c и revert_string.c

--- lab2/src/revert_string/revert_string.c
+++ lab2/src/revert_string/revert_string.c
@@ -2,6 +2,13 @@
 
 void RevertString(char *str)
 {
-	// your code here
+	int n = 0;
+	while (str[n] != '\0') {
+		++n;
+	}
+	for (int i = 0; i < n/2; ++i) {
+		char temp = str[i];
+		str[i] = str[n-1-i];
+		str[n-1-i] = temp;
+	}
 }
-
